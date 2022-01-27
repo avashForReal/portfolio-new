62b6bac033cc0c6946697bce26b4fd9382f5f259
twobits <avash700@gmail.com>
2022-01-27 16:03:52 +0545

    minor fix

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,17 @@
 (function contactForm() {
     const formDom = document.getElementById("contact-form");
-    const submitBtn = formDom.children[3];
+    console.log(formDom.children)
+    const submitBtn = formDom.children[6];
 
     submitBtn.addEventListener("click", (e) => {
       e.preventDefault();
-      const name = formDom.children[0].value;
-      const email = formDom.children[1].value;
-      const message = formDom.children[2].value;
+      const nameError = formDom.children[0]
+      const emailError = formDom.children[0]
+      const messageError = formDom.children[0]
+
+      const name = formDom.children[1].value;
+      const email = formDom.children[3].value;
+      const message = formDom.children[5].value;
       const emailRe =
         /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
   
